64b8de91730e1c2f094f5225b04a6f5345be6726
yash-2k21 <yash.sharma_cs20@gla.ac.in>
2022-01-22 00:36:11 +0530

    DSA Learning Series Binary Search

---
+++ src/com/yash/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.yash;
+
+public class BinarySearch
+{
+    static int  Search(int[] arr , int target)
+    {
+        int start = 0;
+        int end = arr.length - 1 ;
+        while(start <= end)
+        {
+            int mid =  start + (end - start)/2;
+            if(target < arr[mid])
+            {
+                end = mid -1;
+            }
+            else if(target > arr[mid])
+            {
+                start = mid + 1 ;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int[]  arr = {-15,-13,-8,2,3,4,6,8,13,34,56,67};
+        int target = 89;
+        int ans = Search(arr,target);
+        //System.out.println("Hello");
+        System.out.println(ans);
+    }
+}
